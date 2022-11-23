1d0e40e6e34724e4025b6f3c8190f56bd3549e08
Salva Castillo <salvadorfcl85@gmail.com>
2022-11-23 11:29:56 +0100

    Controller añadido

---
+++ product-service/src/main/java/com/nttdata/bootcamp/productservice/controller/Controller.java
@@ -0,0 +1,29 @@
+package com.nttdata.bootcamp.productservice.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller{
+	
+	//Modo 1 via @Autowired
+	@Autowired
+	Environment environment;
+	
+	@GetMapping("/port2")
+	public String getPort2() {
+		return "El puerto del micro es "+ environment.getProperty("local.server.port");
+	}
+	
+	//Modo 2  via @Value
+	@Value("${server.port}")
+	private String port;
+	
+	@GetMapping("/port")
+	public String getPort() {
+		return "El puerto del micro es "+ port;
+	}
+}
